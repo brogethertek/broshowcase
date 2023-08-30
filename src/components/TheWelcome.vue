20ee4e5f86c5a70ac92f765c8b34b1703154d59b
david sokhan sanj <david.sokhan-sanj@epitech.eu>
2023-08-30 16:53:41 +0200

    add point

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,86 +1,660 @@
-<script setup>
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
-</script>
-
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
+  <div class="main">
+     <h1 class="title">Présentation de l'équipe :</h1>
+     <div class="flip">
+       <div class="front"></div>
+       <h2>David Sokhan sanj</h2>
+       <div class="back">
+         <h3>Frontend Web</h3>
+         <p>
+           Développeur côté web de l'application. Développement en Vue.js et
+           travail surtout sur le design et l'utilisation du back-end.
+         </p>
+       </div>
+     </div>
+     
+     <div class="flip3">
+       <div class="front3"></div>
+       <h2>Merlin Jallu</h2>
+       <div class="back3">
+         <h3>Frontend Mobile</h3>
+         <p>
+           Création d'un rendu visuel en React Native pour la partie mobile
+           et tablette. Travail sur la partie entreprise et particulier au
+           niveau de la création et rendu final.
+         </p>
+       </div>
+     </div>
+     <div class="flip4">
+       <div class="front4"></div>
+       <h2>Pablo Etienne</h2>
+       <div class="back4">
+         <h3>Frontend Mobile</h3>
+         <p>
+           Travail sur la partie visuel de l'application coté mobile en React
+           Native, et plus précisement sur l'aspect utilisateur côté EHPAD.
+         </p>
+       </div>
+     </div>
+     <div class="flip5">
+       <div class="front5"></div>
+       <h2>Enzo Ekobo</h2>
+       <div class="back5">
+         <h3>Backend Web/Mobile</h3>
+         <p>Developpement et création des routes côté Backend. Développement en node JS.</p>
+       </div>
+     </div>
+     <div class="flip6">
+       <div class="front6"></div>
+       <h2>Alexandre Descoins</h2>
+       <div class="back6">
+         <h3>Backend Web/Mobile</h3>
+         <p>
+           Création d'une API pour la gestion des activités et des
+           ressources, travaille aussi dans la sécurisation du backend sous
+           node JS.
+         </p>
+       </div>
+     </div>
+     <div class="flip7">
+       <div class="front7"></div>
+       <h2>Xavier Pain</h2>
+       <div class="back7">
+         <h3>Devops</h3>
+         <p>DevOps Optimisation & Automatisation de l'infrastructure.</p>
+       </div>
+     </div>
+     
+     <!-- 
+     <div id="carouselActivities" class="carousel slide" data-bs-ride="carousel">
+       <ol class="carousel-indicators">
+         <li data-bs-target="#carouselActivities" data-bs-slide-to="0" class="active"></li>
+         <li data-bs-target="#carouselActivities" data-bs-slide-to="1"></li>
+         <li data-bs-target="#carouselActivities" data-bs-slide-to="2"></li>
+       </ol>
+     -->
+       <!-- Slides 
+       <div class="carousel-inner">
+         <div class="carousel-item active">
+           <img src="../assets/theatre-rue-freres-troubouchchgenot-1600x656.jpg" class="d-block w-100" alt="Activité 1">
+           <div class="carousel-caption d-none d-md-block">
+             <h5>Théatre</h5>
+             <h5>Théatre de rue avec une petite troupe local. Activité pleine aire en EHPAD</h5>
+           </div>
+         </div>
+         <div class="carousel-item">
+           <img src="../assets/124-cours-de-poterie.jpeg" class="d-block w-100" alt="Activité 2">
+           <div class="carousel-caption d-none d-md-block">
+             <h5>Activité poterie</h5>
+             <h5>Une activité poterie sur place avec une chouette ambiance et une equipe pret a vous données leurs experience pour vous aider en toute convivialité</h5>
+           </div>
+         </div>
+         <div class="carousel-item">
+           <img src="../assets/images.jpg" class="d-block w-100" alt="Activité 3">
+           <div class="carousel-caption d-none d-md-block">
+             <h5>Jeux de societe</h5>
+             <p>Event jeux de société avec des personnes de toutes ages organisé en EHPAD</p>
+           </div>
+         </div>
+       </div>
+       -->
+     
+       <!-- Contrôles 
+       <a class="carousel-control-prev" href="#carouselActivities" role="button" data-bs-slide="prev">
+         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+         <span class="visually-hidden">Précédent</span>
+       </a>
+       <a class="carousel-control-next" href="#carouselActivities" role="button" data-bs-slide="next">
+         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+         <span class="visually-hidden">Suivant</span>
+       </a>
+       -->
+     </div>
+     
 
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
+     <div class="test">
+ <div class="container">
+     <div class="timeline">
+       <ul>
+         <li>
+           <div class="timeline-content">
+             
+             <h1>Présentation du projet</h1>
+             <p>Notre application est une plateforme de mise en relation entre des EHPAD et des particuliers ou des entreprises proposant des activités adaptées aux seniors.
+L'objectif est de favoriser le bien-être et la socialisation des résidents en proposant une grande variété d'activités, allant de la musique à la gymnastique douce en passant par des ateliers créatifs.
+Les particuliers et les entreprises peuvent facilement proposer leurs services et les EHPAD peuvent trouver des activités sur mesure pour leurs résidents.
+Notre application est simple d'utilisation et permet de mettre en relation les différents acteurs en quelques clics.
+Nous sommes convaincus que notre application contribuera à améliorer la qualité de vie des personnes âgées et à favoriser leur épanouissement au sein des EHPAD.</p>
+           </div>
+         </li>
+         <li>
+           <div class="timeline-content">
+             <h1>Prestataire</h1>
+             <p>Vous voulez agrandir votre public aux seniors ? Vous êtes désireux de partager votre savoir-faire de l'amusement et de l'épanouissement a un public trop souvent mis de côté ?
+Vous êtes désireux de devenir une référence au prêt de nombreuses structures ? Alors brogether est fait pour vous ! Crée votre profil et rentrez en contact avec tous nos membres.</p>
+           </div>
+         </li>
+         <li>
+           <div class="timeline-content">
+             <h1>EHPAD</h1>
+             <p>Votre EHPAD est désireuse d'amener joie, amusement et épanouissement a son public ? Vous voulez avoir accès à un panel d'activités divers et varier tout en arrêtant de devoir faire des heures de recherches ?
+Alors il n'y a plus qu'un pas à faire, votre profil sur BROGHER afin d'avoir accès aux nombreux partenaires désireux de vous proposer des nombreuses activités diverses varie et adapté au bien-être de votre public !</p>
+           </div>
+         </li>
+         <li>
+           <div class="timeline-content">
+             <h1>Contact</h1>
+             <p>Une fois, votre profil crée, vous aurez accès au catalogue d'activités vous permettant de rentrer immédiatement en contact avec le prestataire qui vous intéresse.</p>
+           </div>
+         </li>
+       </ul>
+     </div>
+ </div>
+ </div>
+   
+   
 
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
 
-    <br />
 
-    More instructions are available in <code>README.md</code>.
-  </WelcomeItem>
 
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
 
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
 
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
 
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
 
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+ 
 </template>
+
+<style>
+
+.main {
+    height: 600px;
+    width: 100%;
+    padding-top: 150px;
+}
+
+.title {
+    font-size: 5em;
+    text-align: center;
+    font-family: "Montserrat", sans-serif;
+}
+
+.flip {
+    position: relative;
+    top: 0%;
+    left: 50px;
+    height: 200px;
+    width: 200px;
+    text-align: center;
+}
+
+.front,.back {
+    background-color: #EC7703;
+    display: block;
+    width: 200px;
+    height: 200px;
+    border-radius: 10px;
+    transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.front {
+    background-image: url("../assets/dav.jpg");
+    background-position: center;
+    background-size: cover;
+    box-shadow: 0 5px 20px rgba(255, 255, 255, 0.205);
+    transform: rotateY(0);
+}
+
+.back {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    transform: rotateY(-180deg);
+}
+
+.flip:hover .front {
+    transform: rotateY(180deg);
+}
+
+.flip:hover .back {
+    opacity: 1;
+    transform: rotateY(0);
+}
+
+.flip2 {
+    position: relative;
+    top: 0%;
+    left: 350px;
+    height: 200px;
+    width: 200px;
+    text-align: center;
+}
+
+.front2,.back2 {
+    background-color: #EC7703;
+    display: block;
+    width: 200px;
+    height: 200px;
+    border-radius: 10px;
+    transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.front2 {
+    background-image: url("../assets/images.jpg");
+    background-position: center;
+    background-size: cover;
+    box-shadow: 0 5px 20px rgba(255, 255, 255, 0.205);
+    transform: rotateY(0);
+}
+
+.back2 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    transform: rotateY(-180deg);
+}
+
+.flip2:hover .front2 {
+    transform: rotateY(180deg);
+}
+
+.flip2:hover .back2 {
+    opacity: 1;
+    transform: rotateY(0);
+}
+
+.flip3 {
+    position: relative;
+    top: -200px;
+    left: 300px;
+    height: 200px;
+    width: 200px;
+    text-align: center;
+}
+
+.front3,.back3 {
+    background-color: #EC7703;
+    display: block;
+    width: 200px;
+    height: 200px;
+    border-radius: 10px;
+    transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.front3 {
+    background-image: url("../assets/Merlin.jpg");
+    background-position: center;
+    background-size: cover;
+    box-shadow: 0 5px 20px rgba(255, 255, 255, 0.205);
+    transform: rotateY(0);
+}
+
+.back3 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    transform: rotateY(-180deg);
+}
+
+.flip3:hover .front3 {
+    transform: rotateY(180deg);
+}
+
+.flip3:hover .back3 {
+    opacity: 1;
+    transform: rotateY(0);
+}
+
+.flip4 {
+    position: relative;
+    top: -400px;
+    left: 550px;
+    height: 200px;
+    width: 200px;
+    text-align: center;
+}
+
+.front4,.back4 {
+    background-color: #EC7703;
+    display: block;
+    width: 200px;
+    height: 200px;
+    border-radius: 10px;
+    transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.front4 {
+    background-image: url("../assets/pablo.jpg");
+    background-position: center;
+    background-size: cover;
+    box-shadow: 0 5px 20px rgba(255, 255, 255, 0.205);
+    transform: rotateY(0);
+}
+
+.back4 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    transform: rotateY(-180deg);
+}
+
+.flip4:hover .front4 {
+    transform: rotateY(180deg);
+}
+
+.flip4:hover .back4 {
+    opacity: 1;
+    transform: rotateY(0);
+}
+
+.flip5 {
+    position: relative;
+    top: -600px;
+    left: 800px;
+    height: 200px;
+    width: 200px;
+    text-align: center;
+}
+
+.front5,.back5 {
+    background-color: #EC7703;
+    display: block;
+    width: 200px;
+    height: 200px;
+    border-radius: 10px;
+    transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.front5 {
+    background-image: url("../assets/enzo.jpeg");
+    background-position: center;
+    background-size: cover;
+    box-shadow: 0 5px 20px rgba(255, 255, 255, 0.205);
+    transform: rotateY(0);
+}
+
+.back5 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    transform: rotateY(-180deg);
+}
+
+.flip5:hover .front5 {
+    transform: rotateY(180deg);
+}
+
+.flip5:hover .back5 {
+    opacity: 1;
+    transform: rotateY(0);
+}
+
+.flip6 {
+    position: relative;
+    top: -800px;
+    left: 1050px;
+    height: 200px;
+    width: 200px;
+    text-align: center;
+}
+
+.front6,.back6 {
+    background-color: #EC7703;
+    display: block;
+    width: 200px;
+    height: 200px;
+    border-radius: 10px;
+    transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.front6 {
+    background-image: url("../assets/Alex.jpg");
+    background-position: center;
+    background-size: cover;
+    box-shadow: 0 5px 20px rgba(255, 255, 255, 0.205);
+    transform: rotateY(0);
+}
+
+.back6 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    transform: rotateY(-180deg);
+}
+
+.flip6:hover .front6 {
+    transform: rotateY(180deg);
+}
+
+.flip6:hover .back6 {
+    opacity: 1;
+    transform: rotateY(0);
+}
+
+.flip7 {
+    position: relative;
+    top: -1000px;
+    left: 1300px;
+    height: 200px;
+    width: 200px;
+    text-align: center;
+}
+
+.front7,.back7 {
+    background-color: #EC7703;
+    display: block;
+    width: 200px;
+    height: 200px;
+    border-radius: 10px;
+    transition: all .5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.front7 {
+    background-image: url("../assets/xavier.png");
+    background-position: center;
+    background-size: cover;
+    box-shadow: 0 5px 20px rgba(255, 255, 255, 0.205);
+    transform: rotateY(0);
+}
+
+.back7 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0;
+    transform: rotateY(-180deg);
+}
+
+.flip7:hover .front7 {
+    transform: rotateY(180deg);
+}
+
+.flip7:hover .back7 {
+    opacity: 1;
+    transform: rotateY(0);
+}
+
+
+html {
+  font-family: "Montserrat";
+}
+.container {
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 100px 0;
+  background-color: #111;
+}
+.timeline {
+  width: 80%;
+  height: auto;
+  max-width: 800px;
+  margin: 0 auto;
+  position: relative;
+}
+
+.timeline ul {
+  list-style: none;
+}
+.timeline ul li {
+  padding: 20px;
+  background-color: #1e1f22;
+  color: white;
+  border-radius: 10px;
+  margin-bottom: 20px;
+}
+.timeline ul li:last-child {
+  margin-bottom: 0;
+}
+.timeline-content h1 {
+  font-weight: 500;
+  font-size: 25px;
+  line-height: 30px;
+  margin-bottom: 10px;
+}
+.timeline-content p {
+  font-size: 16px;
+  line-height: 30px;
+  font-weight: 300;
+}
+.timeline-content .date {
+  font-size: 12px;
+  font-weight: 300;
+  margin-bottom: 10px;
+  letter-spacing: 2px;
+}
+@media only screen and (min-width: 768px) {
+  .timeline:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 2px;
+    height: 100%;
+    background-color: gray;
+  }
+  .timeline ul li {
+    width: 50%;
+    position: relative;
+    margin-bottom: 50px;
+  }
+  .timeline ul li:nth-child(odd) {
+    float: left;
+    clear: right;
+    transform: translateX(-30px);
+    border-radius: 20px 0px 20px 20px;
+  }
+  .timeline ul li:nth-child(even) {
+    float: right;
+    clear: left;
+    transform: translateX(30px);
+    border-radius: 0px 20px 20px 20px;
+  }
+  .timeline ul li::before {
+    content: "";
+    position: absolute;
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+    background-color: gray;
+    top: 0px;
+  }
+  .timeline ul li:nth-child(odd)::before {
+    transform: translate(50%, -50%);
+    right: -30px;
+  }
+  .timeline ul li:nth-child(even)::before {
+    transform: translate(-50%, -50%);
+    left: -30px;
+  }
+  .timeline-content .date {
+    position: absolute;
+    top: -30px;
+  }
+  .timeline ul li:hover::before {
+    background-color: aqua;
+  }
+
+  .carousel-caption {
+      background-color: rgba(0, 0, 0, 0.5);
+      color: #fff;
+      padding: 1rem;
+    }
+
+    .carousel-control-prev, .carousel-control-next {
+      background-color: rgba(0, 0, 0, 0.5);
+      width: 40px;
+    }
+
+    .carousel-control-prev-icon, .carousel-control-next-icon {
+      width: 20px;
+      height: 20px;
+    }
+
+    .carousel-control-prev-icon:before {
+      content: '\2039';
+      font-size: 24px;
+    }
+
+    .carousel-control-next-icon:before {
+      content: '\203a';
+      font-size: 24px;
+    }
+    .carousel-indicators li {
+      background-color: rgba(255, 255, 255, 0.5);
+      border-radius: 50%;
+      height: 12px;
+      margin: 0 8px;
+      width: 12px;
+    }
+
+    .carousel-indicators .active {
+      background-color: #fff;
+    }
+}
+
+</style>
